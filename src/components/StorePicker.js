014c8058ddb3449fb60ee7142678b1283980ebc5
Lawrence N. Masullo <lmasullo@gmail.com>
2019-08-26 20:51:08 -0500

    Updated on imac

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,15 +1,28 @@
-import React from "react";
+// Need to import React in each component
+import React from 'react';
 
+// This is our Component
+// Create a class - use capital first letter and it 'extends' the Component method of React
 class StorePicker extends React.Component {
+  // Calls render
   render() {
+    // JSX uses className not class
     return (
-      <form className="store-selector">
-        <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" />
-        <button type="submit">Visit Store →</button>
-      </form>
+      // Put stuff in () so we can put on separate line
+      // And go only return one element, so if have multiple elements you must wrap in a div or React.Fragment tag or empty <>
+      //! Looks like normal commenting is working
+      <>
+        <p>Test</p>
+        {/* Curly brackets in jsx means this is javascript */}
+        <form className="store-selector">
+          <h2>Please Enter a Store</h2>
+          <input type="text" required placeholder="Store Name" />
+          <button type="submit">Visit Store</button>
+        </form>
+      </>
     );
   }
 }
 
+// Export so it is available in other files
 export default StorePicker;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-// let's go!
+// React knows to look for the index.js
+// Import React using ES6 modules, from 'react' must be lower case
+import React from 'react';
+// Or just import Components and ext that
+// import {Component} from 'react';
+// class StorePicker extends Component;
+
+// Just importing the render method from the react-dom package
+import { render } from 'react-dom';
+
+// Import the component, don't need the .js
+import StorePicker from './components/StorePicker';
+
+// Can import css here or put in component
+import './css/style.css';
+
+// Render the StorePicker component to the main div on the index.html page
+render(<StorePicker />, document.getElementById('main'));
